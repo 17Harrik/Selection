89a8926da2b1657c2de1166d69c500395e5d0ae2
Kieran Harrison <17HarriK@fulford.york.sch.uk>
2022-09-14 12:04:36 +0100

    Added switch example

--- Selection/Program.cs
+++ Selection/Program.cs
@@ -9,7 +9,33 @@ namespace Selection
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            Console.Write("What pizza topping would you like? ");
+            string pizzaTopping = Console.ReadLine();
+
+            if (pizzaTopping.ToLower() == "ham")
+            {
+                Console.WriteLine("Not suitable for vegetarians");
+            }
+
+            switch(pizzaTopping)
+            {
+                case "ham":
+                case "pepperoni":
+                case "chicken":
+                    Console.WriteLine("Not suitable for vegetarians");
+                    break;
+                case "vegetables":
+                case "cheese and tomato":
+                case "margherita":
+                    Console.WriteLine("Suitable for vegetarians");
+                    break;
+                default:
+                    Console.WriteLine("Unknown pizza topping!");
+                    break;
+            }
+        
+        
         }
     }
 }
